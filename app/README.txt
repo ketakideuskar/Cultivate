32d75628761ad7b03488d03321ff1c96c2810b0a
ketakideuskar <kdeuskar@cs.washington.edu>
2019-10-12 16:50:04 -0700

    Initial commit

---
+++ app/README.txt
@@ -0,0 +1,1 @@
+Welcome to Cultivate, our project for Dubhacks 2019!
--- app/src/main/java/com/example/cultivate/MainActivity.java
+++ app/src/main/java/com/example/cultivate/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cultivate;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
