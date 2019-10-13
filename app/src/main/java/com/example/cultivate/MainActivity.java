4da1303498054255b0d32a838000d88cc85109b6
Paul Druta <drutap@cs.washington.edu>
2019-10-12 20:47:02 -0700

    Added username String to intents

--- app/src/main/java/com/example/cultivate/MainActivity.java
+++ app/src/main/java/com/example/cultivate/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.cultivate;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.*;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.api.Places;
@@ -15,6 +16,11 @@ import com.google.android.libraries.places.api.Places;
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.Cultivate.MESSAGE";
+    private EditText name;
+    private EditText password;
+    private TextView info;
+    private Button button;
+    private HashMap<String, String> passwordMap = new HashMap<String, String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +32,20 @@ public class MainActivity extends AppCompatActivity {
         PlacesClient placesClient = Places.createClient(this);
     }
 
+    public void validate(String userName, String userPassword) {
+        //if (passwordMap.keySet().contains(userName) && passwordMap.get(userName).equals(userPassword)) {
+            Intent intent = new Intent(this, MetroAreaSelect.class);
+            intent.putExtra("Username", userName);
+            startActivity(intent);
+        //}
+    }
+
 
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, MetroAreaSelect.class);
+        intent.putExtra("Username", "Paul");
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/cultivate/MetroAreaSelect.java
+++ app/src/main/java/com/example/cultivate/MetroAreaSelect.java
@@ -1,14 +1,24 @@
 package com.example.cultivate;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MetroAreaSelect extends AppCompatActivity {
 
+    private String userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        userName = intent.getExtras().getString("Username");
         setContentView(R.layout.activity_metro_area_select);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Map.class);
+        intent.putExtra("Username", userName);
+        startActivity(intent);
     }
 }
