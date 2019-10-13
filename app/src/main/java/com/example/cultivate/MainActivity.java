35a452d8fba8947bdad045e53e74d54e96d8db20
Paul Druta <drutap@cs.washington.edu>
2019-10-12 18:01:34 -0700

    Early file navigation

--- app/src/main/java/com/example/cultivate/MainActivity.java
+++ app/src/main/java/com/example/cultivate/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.cultivate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 //DUBHACKS 2019
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.Cultivate.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MetroAreaSelect.class);
+        startActivity(intent);
+    }
 }
+
---
+++ app/src/main/java/com/example/cultivate/MetroAreaSelect.java
@@ -0,0 +1,14 @@
+package com.example.cultivate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MetroAreaSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metro_area_select);
+    }
+}
