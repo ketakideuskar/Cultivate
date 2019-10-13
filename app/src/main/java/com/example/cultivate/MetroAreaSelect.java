a05cdc272fbdd8502945e03cfec25c581c4391ce
Paul Druta <drutap@cs.washington.edu>
2019-10-13 03:33:07 -0700

    Drop Down to Map Implementation

--- app/src/main/java/com/example/cultivate/MetroAreaSelect.java
+++ app/src/main/java/com/example/cultivate/MetroAreaSelect.java
@@ -10,6 +10,8 @@ import android.widget.Spinner;
 public class MetroAreaSelect extends AppCompatActivity {
 
     private String userName;
+    private String city;
+    private Spinner mySpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,16 +19,18 @@ public class MetroAreaSelect extends AppCompatActivity {
         userName = intent.getExtras().getString("Username");
         setContentView(R.layout.activity_metro_area_select);
 
-        Spinner mySpinner = (Spinner) findViewById(R.id.location_spinner);
+        mySpinner = (Spinner) findViewById(R.id.location_spinner);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MetroAreaSelect.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
+
     }
 
     public void sendMessage(View view) {
+        city = mySpinner.getSelectedItem().toString();
         Intent intent = new Intent(this, Map.class);
         intent.putExtra("Username", userName);
-        intent.putExtra("City", "Seattle");
+        intent.putExtra("City", city);
         startActivity(intent);
     }
 }
