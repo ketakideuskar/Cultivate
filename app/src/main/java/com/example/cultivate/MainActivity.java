a755ac39aeb58fdfff871e5c41d8a7cf5fd1cf85
ketakideuskar <kdeuskar@cs.washington.edu>
2019-10-12 18:41:00 -0700

    imported places api

--- app/src/main/java/com/example/cultivate/MainActivity.java
+++ app/src/main/java/com/example/cultivate/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.api.Places;
 
 //DUBHACKS 2019
 public class MainActivity extends AppCompatActivity {
@@ -17,12 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize the SDK
+        Places.initialize(getApplicationContext(), "AIzaSyDp7RDmSHrZ-tx3ezwafjlAAPzN-VQqBhs");
+
+// Create a new Places client instance
+        PlacesClient placesClient = Places.createClient(this);
     }
+
+
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, MetroAreaSelect.class);
         startActivity(intent);
     }
+
 }
 
